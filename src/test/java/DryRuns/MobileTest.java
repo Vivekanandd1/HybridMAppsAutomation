e0084d4c047a03d7926a6f5d2ab96227db6b6752
vivkekanandd1 <vivekanandd1@live.com>
2025-08-06 14:16:12 +0530

    E2E execution

--- src/test/java/DryRuns/MobileTest.java
+++ src/test/java/DryRuns/MobileTest.java
@@ -9,9 +9,15 @@ import io.appium.java_client.android.AndroidDriver;
 public class MobileTest extends BaseClass {
 	
 	
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		AndroidDriver driver = Capabilities();
-		driver.findElement(By.xpath("//android.widget.TextView[@text='Preference']")).click();
+		 driver.findElement(By.xpath("//android.widget.TextView[@text='Preference']")).click();
+		 driver.findElement(By.xpath("//android.widget.TextView[@text='3. Preference dependencies']")).click();
+		 driver.findElement(By.id("android:id/checkbox")).click();
+		 Thread.sleep(2000);
+		 driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+		 driver.findElement(By.className("android.widget.EditText")).sendKeys("VivekVats");
+		 driver.findElements(By.className("android.widget.Button")).get(1).click();
 	}
 
 }

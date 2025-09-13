82d793fadf49b102bd28ec0435ae9d1084b48daa
vivkekanandd1 <vivekanandd1@live.com>
2025-09-13 21:27:28 +0530

    Real Device invokation codes

---
+++ src/test/java/DryRuns/BrowserInvokation.java
@@ -0,0 +1,29 @@
+package DryRuns;
+
+import java.net.MalformedURLException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.appium.java_client.android.AndroidDriver;
+
+public class BrowserInvokation extends BrowserClass {
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws MalformedURLException, InterruptedException  {
+		AndroidDriver driver = Capabilities("Emulator");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(130));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("admin123");
+		WebElement Login = driver.findElement(By.xpath("//button[@type='submit']"));
+		wait.until(ExpectedConditions.elementToBeClickable(Login));
+		Login.click();
+		System.out.println("Clicked");
+	}
+
+}
